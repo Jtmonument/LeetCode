e13a8bd1ed09132173818debfdebd9fd84ddbdfc
jonta <jontaylor091@gmail.com>
2022-05-24 22:16:35 -0500

    Height Checker

---
+++ Arrays 101/HeightChecker.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class HeightChecker {
+    public int heightChecker(int[] heights) {
+        int[] copy = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(copy);
+        int count = 0;
+        for(int i = 0; i < heights.length; i++) {
+            if(copy[i] != heights[i])
+                count++;
+        }
+        return count;
+    }
+}
