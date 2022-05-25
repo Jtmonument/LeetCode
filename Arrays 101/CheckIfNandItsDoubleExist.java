ac48131caf98ca4fcf95d77f98594270188f7874
jonta <jontaylor091@gmail.com>
2022-05-24 22:16:21 -0500

    Check if N and its Double Exist

---
+++ Arrays 101/CheckIfNandItsDoubleExist.java
@@ -0,0 +1,19 @@
+class CheckIfNandItsDoubleExist {
+    public boolean checkIfExist(int[] arr) {
+        int check = arr[0] * 2;
+        for(int n = 0, m = 0; n < arr.length; m++) {
+            if(m == arr.length) {
+                m = 0;
+                if(n != arr.length - 1) {
+                    check = arr[++n] * 2;
+                } else {
+                    break;
+                }
+            }
+            if(check == arr[m] && n != m) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
