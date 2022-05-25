65f1f716899fbc0fc626904188b7d2d74c2513c5
jonta <jontaylor091@gmail.com>
2022-05-24 22:19:31 -0500

    Remove Element

---
+++ Arrays 101/RemoveElement.java
@@ -0,0 +1,14 @@
+class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int length = nums.length;
+        for(int i = 0, j = 0; i < nums.length; i++) {
+            if(nums[i] == val) {
+                nums[j] = 0;
+                length--;
+            } else {
+                nums[j++] = nums[i];
+            }
+        }
+        return length;
+    }
+}
