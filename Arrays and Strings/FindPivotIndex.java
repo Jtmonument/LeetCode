cdbe32e0801fb36f0c40e859e49fc2533e3b7e2b
jonta <jontaylor091@gmail.com>
2022-05-26 22:21:44 -0500

    Find Pivot Index

---
+++ Arrays and Strings/FindPivotIndex.java
@@ -0,0 +1,17 @@
+public class FindPivotIndex {
+    public int pivotIndex(int[] nums) {
+        if (nums.length < 3) {return -1;}
+        int left = nums[0];
+        int right = 0;
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i; j < nums.length; j++) {
+                right += nums[j];
+            }
+            if(left == right) {return i;}
+            right = 0;
+            if(i + 1 == nums.length) {break;}
+            left += nums[i + 1];
+        }
+        return -1;
+    }
+}
