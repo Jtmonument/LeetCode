10fa20a0fcf583f7eab6677c27348a8ad80c7c84
jonta <jontaylor091@gmail.com>
2022-05-28 11:22:58 -0500

    Delete Node In A LinkedList

---
+++ Top Interview Questions/easycollection/DeleteNodeInALinkedList.java
@@ -0,0 +1,15 @@
+package easycollection;
+
+public class DeleteNodeInALinkedList {
+
+    private static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
