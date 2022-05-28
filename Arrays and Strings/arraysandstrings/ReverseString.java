2d46ba905412235cc2b453d5f922c0f4b467a50e
jonta <jontaylor091@gmail.com>
2022-05-28 10:50:28 -0500

    Reverse String

---
+++ Arrays and Strings/arraysandstrings/ReverseString.java
@@ -0,0 +1,12 @@
+package arraysandstrings;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        char temp;
+        for(int i = s.length - 1, j = 0; i >= s.length / 2; i--, j++) {
+            temp = s[j];
+            s[j] = s[i];
+            s[i] = temp;
+        }
+    }
+}
