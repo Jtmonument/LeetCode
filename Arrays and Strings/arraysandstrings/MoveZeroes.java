f3f1ef6fdac703815b0f83e52797724035538095
jonta <jontaylor091@gmail.com>
2022-05-28 10:49:42 -0500

    Move Zeroes

---
+++ Arrays and Strings/arraysandstrings/MoveZeroes.java
@@ -0,0 +1,17 @@
+package arraysandstrings;
+
+class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int writePointer = 0;
+        int zeroes = 0;
+        for (int readPointer = 0; readPointer < nums.length; readPointer++) {
+            if (nums[readPointer] != 0) {
+                nums[writePointer] = nums[readPointer];
+                writePointer++;
+            } else {zeroes++;}
+        }
+        for(int i = nums.length - 1; i != (nums.length  - 1)- zeroes; i--) {
+            nums[i] = 0;
+        }
+    }
+}
