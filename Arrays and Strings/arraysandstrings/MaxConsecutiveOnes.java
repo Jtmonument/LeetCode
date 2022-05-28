9e0319998245aefbdb306c05578587be0a72c9f2
jonta <jontaylor091@gmail.com>
2022-05-28 10:43:05 -0500

    Max Consecutive Ones

--- Arrays and Strings/arraysandstrings/MaxConsecutiveOnes.java
+++ Arrays and Strings/arraysandstrings/MaxConsecutiveOnes.java
@@ -1,4 +1,4 @@
-/*Problem #485: Given a binary array, find the maximum number of consecutive 1s in this array.
+package arraysandstrings;/*Problem #485: Given a binary array, find the maximum number of consecutive 1s in this array.
     Example 1:
 Input: [1,1,0,1,1,1]
 Output: 3
