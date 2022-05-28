616a464d07019c9f49d5bc703e3230ccd780b9ee
jonta <jontaylor091@gmail.com>
2022-05-28 11:23:59 -0500

    Merge Sorted Array

---
+++ Top Interview Questions/easycollection/MergeSortedArray.java
@@ -0,0 +1,11 @@
+package easycollection;
+
+import java.util.Arrays;
+
+class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = m, j = 0; i < nums1.length; i++)
+            nums1[i] = nums2[j++];
+        Arrays.sort(nums1);
+    }
+}
